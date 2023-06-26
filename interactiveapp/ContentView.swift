f1aaef961b98d98b399c154e52f966f222e86033
Aditi Gupta <guptaditi232008@gmail.com>
2023-06-26 10:29:32 -0700

    Initial Commit

---
+++ interactiveapp/ContentView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    
+    var body: some View {
+        VStack (spacing: 50) {
+            Text("What is your name?")
+                .font(.title)
+            TextField("", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+                .padding()
+            Button("Submit Name:") {
+                textTitle = "Welcome \(name)"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ interactiveapp/interactiveappApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveappApp.swift
+//  interactiveapp
+//
+//  Created by Aditi Gupta on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct interactiveappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
